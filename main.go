63fb235388e9f4cda8ee986473cb42c02fca515e
Bruno Hoffmann <brunohoffmanndev@gmail.com>
2023-10-03 19:49:07 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/BrunoHoffmann/api-go/router"
+import (
+	"github.com/BrunoHoffmann/api-go/config"
+	"github.com/BrunoHoffmann/api-go/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	// Initialize Router
 	router.Initialize()
 }
